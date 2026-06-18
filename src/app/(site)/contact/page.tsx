ab3743b81288010deb838b357cfede096e44ec56
yenok-edison <yenok@iktaraa.com>
2026-06-18 19:41:24 +0530

    Contact

--- src/app/(site)/contact/page.tsx
+++ src/app/(site)/contact/page.tsx
@@ -17,8 +17,8 @@ const page = () => {
   return (
     <>
       <HeroSub
-        // title="Contact Us"
-        // description="Discover a wealth of insightful materials meticulously crafted to provide you with a comprehensive understanding of the latest trends."
+        title=""
+        description=""
         breadcrumbLinks={breadcrumbLinks}
       />
       {/* <ContactInfo /> */}
